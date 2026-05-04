87420c698369286069a6321c51dea4121312d43d
carmelinelibongi@gmail.com <you@example.com>
2026-05-04 19:55:08 +0200

    I have just started with buildin my calculator

---
+++ index.js
@@ -0,0 +1,4 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operator]')
+const equalsButtons = document.querySelectorAll('[data-number]')
+const deleteButtons = document.querySelectorAll('[data-delete]')
